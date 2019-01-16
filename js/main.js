2c304068132ec5c6a7dc0d0283fa7b679fa79da8
Cecilia De La Fuente <cid41fg@gmail.com>
2019-01-15 22:34:03 -0500

    update start tile

--- js/main.js
+++ js/main.js
@@ -284,7 +284,7 @@ MainMenuState.preload = function () {
 	this.game.load.image('ground', 'images/ground.png');
 	//////////////////////////////
     this.game.load.image('button', 'images/websitelogo1.png');
-    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
+    this.game.load.image('grass:6x1', 'images/starttile.png');
     this.game.load.spritesheet('hero', 'images/hero.png', 36, 42);
     
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
